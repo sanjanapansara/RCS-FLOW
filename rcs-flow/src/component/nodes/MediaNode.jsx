3b4e6585bbc6e17abe9b80ed2e856feac2bad1f4
Sanjana  Pansara <139979917+sanjanapansara@users.noreply.github.com>
2024-12-06 13:48:59 +0530

    media in card add

--- rcs-flow/src/component/nodes/MediaNode.jsx
+++ rcs-flow/src/component/nodes/MediaNode.jsx
@@ -1,5 +1,5 @@
 import { Handle, Position } from "@xyflow/react";
-import { Badge, Card, ConfigProvider, Switch } from "antd";
+import { Badge, Card, ConfigProvider, Image, Switch, Typography } from "antd";
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
 
@@ -14,18 +14,20 @@ function MediaNode({ data, selected }) {
       theme={{
         components: {
           Card: {
-            headerBg: !enabled ? "#f0f0f0" :"#CACACA",
-            colorBorderSecondary: "#ADC0A7",
+            headerBg: !enabled ? "#f0f0f0" : "#CACACA",
+            colorBorderSecondary: "#CACACA",
           },
         },
       }}
     >
-      {alldata?.data?.isStartNode ? (
+      {alldata?.data?.isStartNode && (
         <Badge.Ribbon
           text={<div className="flex justify-start m-1">Start</div>}
           placement="start"
           style={{ marginTop: -30 }}
-        >
+        />
+      )}
+       
           <Card
             title={alldata?.data?.templateName ?? "Media"}
             size="small"
@@ -38,21 +40,11 @@ function MediaNode({ data, selected }) {
                 onChange={(value) => setEnabled(value)}
               />
             }
-            cover={
-              <img
-                alt="example"
-                src={
-                  alldata?.data?.mediaUrl ??
-                   "https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630"
-                }
-              />
-            }
             bodyStyle={{ padding: "10px" }}
             style={{
               width: 200,
-              padding: "1px",
               boxShadow: "0 2px 6px rgba(0,0,0,0.15)",
-              border: selected ? "1px solid#ADC0A7" : "none",
+              border: selected ? "1px solid#CACACA" : "none",
             }}
           >
             {alldata?.data?.isStartNode ? (
@@ -81,61 +73,51 @@ function MediaNode({ data, selected }) {
             )}
             <div
               style={{ display: "flex", flexDirection: "column" }}
-              dangerouslySetInnerHTML={{
-                __html:
-                  alldata?.data?.label?.replace(/\n/g, "<br/>") || "message",
-              }}
-            />
+              // dangerouslySetInnerHTML={{
+              //   __html:
+              //     alldata?.data?.label?.replace(/\n/g, "<br/>") || "message",
+              // }}
+            >
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  // width: "100%",
+                  border: "1px solid #d9d9d9",
+                  borderRadius: "8px",
+                  padding: "15px",
+                  backgroundColor: "#f9f9f9",
+                }}
+              >
+                <Image
+                  preview={false}
+                  alt="example"
+                  src={
+                    alldata?.data?.mediaUrl ||
+                    "https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630"
+                  }
+                  style={{
+                    marginBottom: "10px",
+                    borderRadius: "4px",
+                    width: "100%" 
+                  }}
+                />
+                <Typography.Text style={{ whiteSpace: "pre-wrap" }}>
+            {alldata?.data?.label
+              ? alldata?.data?.label.split("\n").map((line, index) => (
+                  <span key={index}>
+                    {line}
+                    <br />
+                  </span>
+                ))
+              : "No description available."}
+          </Typography.Text>
+              </div>
+            </div>
           </Card>
-        </Badge.Ribbon>
-      ) : (
-        <Card
-          title={alldata?.data?.templateName ?? "Media"}
-          size="small"
-          extra={
-            <Switch
-              size="small"
-              disabled={alldata?.data?.isStartNode && true}
-              checked={enabled}
-              value={enabled}
-              onChange={(value) => setEnabled(value)}
-            />
-          }
-          cover={
-            <img
-              alt="example"
-              src={
-                alldata?.data?.mediaUrl ??
-                "https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630"
-              }
-            />
-          }
-          bodyStyle={{ padding: "10px" }}
-          style={{
-            width: 200,
-            padding: "1px",
-            boxShadow: "0 2px 6px rgba(0,0,0,0.15)",
-            border: selected ? "1px solid #ADC0A7" : "none",
-            backgroundColor: !enabled ? "#f0f0f0" : "white",
-            color: !enabled ? "#aaa" : "black",
-          }}
-        >
-          {enabled && (
-            <Handle
-              type="target"
-              position={Position.Left}
-              isConnectable={true}
-            />
-          )}
-          <div
-            style={{ display: "flex", flexDirection: "column" }}
-            dangerouslySetInnerHTML={{
-              __html:
-                alldata?.data?.label?.replace(/\n/g, "<br/>") || "message",
-            }}
-          />
-        </Card>
-      )}
+       
+     
     </ConfigProvider>
   );
 }
--- rcs-flow/src/component/nodes/TextNode.jsx
+++ rcs-flow/src/component/nodes/TextNode.jsx
@@ -20,12 +20,14 @@ const TextNode = ({ data, selected }) => {
         },
       }}
     >
-      {alldata?.data?.isStartNode ? (
+      {alldata?.data?.isStartNode && (
         <Badge.Ribbon
           text={<div className="flex justify-start m-1">Start</div>}
           placement="start"
           style={{ marginTop: -30 }}
-        >
+        />
+      )}
+      
           <Card
             title={data?.templateName ?? "Send Message"}
             extra={
@@ -71,6 +73,7 @@ const TextNode = ({ data, selected }) => {
                 )}
               </>
             )}
+            <center>
             <Typography.Text style={{ whiteSpace: "pre-wrap" }}>
               {data.label
                 ? data.label.split("\n").map((line, index) => (
@@ -81,66 +84,8 @@ const TextNode = ({ data, selected }) => {
                   ))
                 : "No label available."}
             </Typography.Text>
-          </Card>
-        </Badge.Ribbon>
-      ) : (
-        <Card
-        title={data?.templateName ?? "Send Message"}
-          extra={
-            <Switch
-              size="small"
-              
-              checked={enabled}
-              value={enabled}
-              onChange={() => setEnabled(!enabled)}
-            />
-          }
-          size="small"
-          bodyStyle={{ padding: "10px" }}
-          style={{
-            width: 200,
-            padding: "0px",
-            borderRadius: 10,
-            boxShadow: "0 2px 6px rgba(0,0,0,0.15)",
-            border: selected ? "1px solid #D1AFAF" : "none",
-          }}
-        >
-          {alldata?.data?.isStartNode ? (
-            <Handle
-              type={alldata?.data?.isStartNode ? "source" : "target"}
-              position={
-                 Position.Left
-              }
-              isConnectable={true}
-            />
-          ) : (
-            <>
-              <Handle
-                type="target"
-                position={Position.Left}
-                isConnectable={true}
-              />
-              {!enabled && (
-                <Handle
-                  type="source"
-                  position={Position.Left}
-                  isConnectable={true}
-                />
-              )}
-            </>
-          )}
-          <Typography.Text style={{ whiteSpace: "pre-wrap" }}>
-            {data.label
-              ? data.label.split("\n").map((line, index) => (
-                  <span key={index}>
-                    {line}
-                    <br />
-                  </span>
-                ))
-              : "No label available."}
-          </Typography.Text>
-        </Card>
-      )}
+            </center>
+          </Card>     
     </ConfigProvider>
   );
 };
